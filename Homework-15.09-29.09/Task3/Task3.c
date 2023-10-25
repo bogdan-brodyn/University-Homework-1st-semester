556a9036a4cec540bbcbbf8a8683b932a6611c71
bogdan-brodyn <mashina0oshibok@gmail.com>
2023-10-25 20:15:18 +0300

    Resolve conversation

--- Homework-15.09-29.09/Task3/Task3.c
+++ Homework-15.09-29.09/Task3/Task3.c
@@ -3,18 +3,28 @@
 #include <stdbool.h>
 #include <time.h>
 
-void swapArrayElements(int* array, int index1, int index2)
+#define TEST_ARRAY_SIZE 100000
+
+typedef enum
+{
+    defaultErrorCode,
+    memoryLack,
+    countingSortLimitExceeded,
+    testFailed
+} ErrorCode;
+
+void swapArrayElements(int* array, size_t index1, size_t index2)
 {
     int temp = array[index1];
     array[index1] = array[index2];
     array[index2] = temp;
 }
 
-void bubbleSort(int* array, int arraySize)
+void bubbleSort(int* array, size_t arraySize)
 {
-    for (int i = 0; i < arraySize; ++i)
+    for (size_t i = 0; i < arraySize; ++i)
     {
-        for (int j = i + 1; j < arraySize; ++j)
+        for (size_t j = i + 1; j < arraySize; ++j)
         {
             if (array[i] >= array[j])
             {
@@ -24,28 +34,32 @@ void bubbleSort(int* array, int arraySize)
     }
 }
 
-int countingSort(int* array, int arraySize)
+ErrorCode countingSort(int* array, size_t arraySize)
 {
-    int* arrayElementsCount = (int*)malloc(sizeof(int) * arraySize);
-    for (int i = 0; i < arraySize; ++i)
+    int* arrayElementsCount = (int*)calloc(arraySize, sizeof(int));
+    if (arrayElementsCount == NULL)
+    {
+        return memoryLack;
+    }
+    for (size_t i = 0; i < arraySize; ++i)
     {
         arrayElementsCount[i] = 0;
     }
 
-    for (int i = 0; i < arraySize; ++i)
+    for (size_t i = 0; i < arraySize; ++i)
     {
         if (array[i] >= arraySize)
         {
             free(arrayElementsCount);
-            return 1;
+            return countingSortLimitExceeded;
         }
         ++arrayElementsCount[array[i]];
     }
 
-    int fillingStart = 0;
-    for (int element = 0; element < arraySize; ++element)
+    size_t fillingStart = 0;
+    for (size_t element = 0; element < arraySize; ++element)
     {
-        for (int filledElementsCount = 0; filledElementsCount < arrayElementsCount[element]; ++filledElementsCount)
+        for (size_t filledElementsCount = 0; filledElementsCount < arrayElementsCount[element]; ++filledElementsCount)
         {
             array[fillingStart + filledElementsCount] = element;
         }
@@ -53,39 +67,60 @@ int countingSort(int* array, int arraySize)
     }
 
     free(arrayElementsCount);
+    return defaultErrorCode;
+}
+
+ErrorCode getTwoTestArraysSpace(int** testArray1, int** testArray2, size_t testArraySize)
+{
+    *testArray1 = (int*)malloc(sizeof(int) * testArraySize);
+    *testArray2 = (int*)malloc(sizeof(int) * testArraySize);
+    if (*testArray1 == NULL || *testArray2 == NULL)
+    {
+        free(*testArray1);
+        free(*testArray2);
+        *testArray1 = NULL;
+        *testArray2 = NULL;
+        return memoryLack;
+    }
+    srand(time(NULL));
+    for (int i = 0; i < testArraySize; ++i)
+    {
+        testArray1[i] = testArray2[i] = rand() % testArraySize;
+    }
+    return defaultErrorCode;
 }
 
 bool testSortsWorkTheSame(void)
 {
-    int arraySize = 1000;
-    int* array1 = (int*)malloc(sizeof(int) * arraySize);
-    int* array2 = (int*)malloc(sizeof(int) * arraySize);
-    for (int i = 0; i < arraySize; ++i)
+    const int arraySize = 1000;
+    int* arrayForBubbleSort = NULL;
+    int* arrayForCountSort = NULL;
+    if (getTwoTestArraysSpace(&arrayForBubbleSort, &arrayForCountSort, arraySize) != defaultErrorCode)
     {
-        array1[i] = array2[i] = rand() % arraySize;
-    }
-
-    bubbleSort(array1, arraySize);
-    int errorCode = countingSort(array2, arraySize);
-    if (errorCode != 0)
-    {
-        free(array1);
-        free(array2);
         return false;
     }
-    for (int i = 0; i < arraySize; ++i)
+
+    bool testResult = true;
+    bubbleSort(arrayForBubbleSort, arraySize);
+    int errorCode = countingSort(arrayForCountSort, arraySize);
+    if (errorCode != 0)
     {
-        if (!((i == arraySize - 1 || array1[i] <= array1[i + 1]) && array1[i] == array2[i]))
+        testResult = false;
+    }
+    else
+    {
+        for (int i = 0; i < arraySize; ++i)
         {
-            free(array1);
-            free(array2);
-            return false;
+            if (!((i == arraySize - 1 || arrayForBubbleSort[i] <= arrayForBubbleSort[i + 1]) 
+                && arrayForBubbleSort[i] == arrayForCountSort[i]))
+            {
+                testResult = false;
+            }
         }
     }
-
-    free(array1);
-    free(array2);
-    return true;
+    free(arrayForBubbleSort);
+    free(arrayForCountSort);
+    return testResult;
 }
 
 int main(void)
@@ -93,42 +128,35 @@ int main(void)
     if (!testSortsWorkTheSame())
     {
         printf("Sorry, program is working incorrectly\n");
-        return 1;
+        return testFailed;
     }
     printf("Program is working correctly\n");
-
-    int testArraySize = 100000;
-    int* testArray1 = (int*)malloc(sizeof(int) * testArraySize);
-    int* testArray2 = (int*)malloc(sizeof(int) * testArraySize);
-    if (testArray1 == NULL || testArray2 == NULL)
+    
+    int* testArray1 = NULL;
+    int* testArray2 = NULL;
+    if (getTwoTestArraysSpace(&testArray1, &testArray2, TEST_ARRAY_SIZE) != defaultErrorCode)
     {
         printf("Sorry, program didn't have enough memory to run\n");
-        free(testArray1);
-        free(testArray2);
-        return 2;
-    }
-    for (int i = 0; i < testArraySize; ++i)
-    {
-        testArray1[i] = testArray2[i] = rand() % testArraySize;
+        return memoryLack;
     }
 
-    printf("Sorting an array of length %d took:\n", testArraySize);
+    printf("Sorting an array of length %d took:\n", TEST_ARRAY_SIZE);
     clock_t begin = clock();
-    bubbleSort(testArray1, testArraySize);
+    bubbleSort(testArray1, TEST_ARRAY_SIZE);
     clock_t end = clock();
-    printf("Bubble sort %d clocks\n", end - begin);
+    printf("Bubble sort took %d clocks\n", end - begin);
 
     begin = clock();
-    int errorCode = countingSort(testArray2, testArraySize);
+    int errorCode = countingSort(testArray2, TEST_ARRAY_SIZE);
     end = clock();
     if (errorCode != 0)
     {
         printf("Counting sort failed\n");
         free(testArray1);
         free(testArray2);
-        return 3;
+        return countingSortLimitExceeded;
     }
-    printf("Counting sort %d clocks\n", end - begin);
+    printf("Counting sort took %d clocks\n", end - begin);
 
     free(testArray1);
     free(testArray2);
