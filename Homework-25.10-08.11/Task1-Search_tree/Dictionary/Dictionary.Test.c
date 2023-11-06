9d28504fac07e8e326353ba63408962177a4bbb0
bogdan-brodyn <mashina0oshibok@gmail.com>
2023-11-06 23:39:23 +0300

    Add first task: Search tree

---
+++ Homework-25.10-08.11/Task1-Search_tree/Dictionary/Dictionary.Test.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Dictionary.h"
+
+typedef enum
+{
+    testPassed,
+    testFailed
+} TestResult;
+
+static TestResult getAndCompareValue(const Dictionary* const dictionary, 
+    const int key, const char* const correctValue)
+{
+    char* value = NULL;
+    int errorCode = get(dictionary, key, &value);
+    for (size_t i = 0; ; ++i)
+    {
+        if (correctValue[i] == '\0' || value[i] == '\0')
+        {
+            return correctValue[i] == value[i] ? testPassed : testFailed;
+        }
+        if (correctValue[i] != value[i])
+        {
+            return testFailed;
+        }
+    }
+    return testFailed;
+}
+
+static TestResult test1(void)
+{
+    Dictionary* dictionary = NULL;
+    bool testResult =
+        add(&dictionary, 2, "b") == dictionaryDefaultErrorCode
+        && add(&dictionary, 1, "a") == dictionaryDefaultErrorCode
+        && add(&dictionary, 3, "c") == dictionaryDefaultErrorCode
+        && add(&dictionary, 4, "d") == dictionaryDefaultErrorCode
+        && getAndCompareValue(dictionary, 1, "a") == testPassed
+        && getAndCompareValue(dictionary, 2, "b") == testPassed
+        && getAndCompareValue(dictionary, 3, "c") == testPassed
+        && getAndCompareValue(dictionary, 4, "d") == testPassed;
+    removeKey(dictionary, 3);
+    testResult &=
+        getAndCompareValue(dictionary, 1, "a") == testPassed
+        && getAndCompareValue(dictionary, 2, "b") == testPassed
+        && getAndCompareValue(dictionary, 4, "d") == testPassed;
+    removeKey(dictionary, 2);
+    testResult &=
+        getAndCompareValue(dictionary, 1, "a") == testPassed
+        && getAndCompareValue(dictionary, 4, "d") == testPassed;
+    deleteDictionary(&dictionary);
+    return testResult && dictionary == NULL ? testPassed : testFailed;
+}
+
+static TestResult test2(void)
+{
+    Dictionary* dictionary = NULL;
+    char* value = NULL;
+    bool testResult =
+        add(NULL, 1, "a") == dictionaryNullPointer
+        && add(&dictionary, 1, "a") == dictionaryDefaultErrorCode
+        && add(&dictionary, 1, "a") == dictionaryUsedKey
+        && get(NULL, 1, NULL) == dictionaryNullPointer
+        && get(dictionary, 1, value) == dictionaryNonExistentKey;
+    deleteDictionary(&dictionary);
+    return testResult && dictionary == NULL ? testPassed : testFailed;
+}
+
+int main(void)
+{
+    TestResult test1Result = test1();
+    printf(test1Result == testPassed ? "Program has passed the first test\n" : 
+        "Program has failed the first test\n");
+    TestResult test2Result = test2();
+    printf(test2Result == testPassed ? "Program has passed the second test\n" :
+        "Program has failed the second test\n");
+    return test1Result == testPassed 
+        && test2Result == testPassed ? testPassed : testFailed;
+}
---
+++ Homework-25.10-08.11/Task1-Search_tree/Dictionary/Dictionary.c
@@ -0,0 +1,220 @@
+﻿#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "Dictionary.h"
+
+typedef struct Node
+{
+    int key;
+    char* value;
+    struct Node* leftChild;
+    struct Node* rightChild;
+} Node;
+
+struct Dictionary
+{
+    Node* root;
+};
+
+static Node* getNext(const Node* const currentElement, const int key)
+{
+    if (currentElement->key < key)
+    {
+        return currentElement->rightChild;
+    }
+    if (currentElement->key > key)
+    {
+        return currentElement->leftChild;
+    }
+    return currentElement;
+}
+
+DictionaryErrorCode add(Dictionary** const dictionary, 
+    const int key, const char* const value)
+{
+    if (dictionary == NULL)
+    {
+        return dictionaryNullPointer;
+    }
+
+    Node* const newElement = (Node* const)calloc(1, sizeof(Node));
+    if (newElement == NULL)
+    {
+        return dictionaryMemoryLack;
+    }
+    newElement->key = key;
+    const size_t valueSize = strlen(value) + 1;
+    newElement->value = (char*)malloc(valueSize * sizeof(char));
+    if (newElement->value == NULL)
+    {
+        free(newElement);
+        return dictionaryMemoryLack;
+    }
+    strcpy_s(newElement->value, valueSize, value);
+
+    if (*dictionary == NULL)
+    {
+        *dictionary = (Dictionary* const)calloc(1, sizeof(Dictionary));
+        if (*dictionary == NULL)
+        {
+            free(newElement->value);
+            free(newElement);
+            return dictionaryMemoryLack;
+        }
+        (*dictionary)->root = newElement;
+        return dictionaryDefaultErrorCode;
+    }
+
+    for (Node* currentElement = (*dictionary)->root; ; 
+        currentElement = getNext(currentElement, key))
+    {
+        if (currentElement->key == key)
+        {
+            free(newElement->value);
+            free(newElement);
+            return dictionaryUsedKey;
+        }
+        if (currentElement->key < key && currentElement->rightChild == NULL)
+        {
+            currentElement->rightChild = newElement;
+            break;
+        }
+        if (currentElement->key > key && currentElement->leftChild == NULL)
+        {
+            currentElement->leftChild = newElement;
+            break;
+        }
+    }
+    return dictionaryDefaultErrorCode;
+}
+
+DictionaryErrorCode get(const Dictionary* const dictionary, 
+    const int key, char** const value)
+{
+    if (dictionary == NULL || value == NULL)
+    {
+        return dictionaryNullPointer;
+    }
+    for (Node* currentElement = dictionary->root; currentElement != NULL;
+        currentElement = getNext(currentElement, key))
+    {
+        if (currentElement->key == key)
+        {
+            *value = currentElement->value;
+            return dictionaryDefaultErrorCode;
+        }
+    }
+    return dictionaryNonExistentKey;
+}
+
+bool isContained(const Dictionary* const dictionary, const int key)
+{
+    if (dictionary == NULL)
+    {
+        return false;
+    }
+    for (Node* currentElement = dictionary->root; currentElement != NULL;
+        currentElement = getNext(currentElement, key))
+    {
+        if (currentElement->key == key)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+static void movePointers(Dictionary* const dictionary, 
+    const Node* const node, Node* const parent)
+{
+    if (node->leftChild == NULL && node->rightChild == NULL)
+    {
+        if (dictionary->root == node)
+        {
+            dictionary->root = NULL;
+        }
+        else if (parent->key < node->key)
+        {
+            parent->rightChild = NULL;
+        }
+        else
+        {
+            parent->leftChild = NULL;
+        }
+    }
+    else if (node->leftChild != NULL && node->rightChild == NULL)
+    {
+
+        if (dictionary->root == node)
+        {
+            dictionary->root = node->leftChild;
+        }
+        else if (parent->key < node->key)
+        {
+            parent->rightChild = node->leftChild;
+        }
+        else
+        {
+            parent->leftChild = node->leftChild;
+        }
+    }
+    else if (node->leftChild == NULL && node->rightChild != NULL)
+    {
+        if (dictionary->root == node)
+        {
+            dictionary->root = node->rightChild;
+        }
+        else if (parent->key < node->key)
+        {
+            parent->rightChild = node->rightChild;
+        }
+        else
+        {
+            parent->leftChild = node->rightChild;
+        }
+    }
+}
+
+void removeKey(Dictionary* const dictionary, const int key)
+{
+    if (dictionary == NULL || !isContained(dictionary, key))
+    {
+        return;
+    }
+    Node* parent = dictionary->root;
+    for (; getNext(parent, key)->key != key; parent = getNext(parent, key));
+    Node* nodeToRemove = getNext(parent, key);
+    if (nodeToRemove->leftChild != NULL && nodeToRemove->rightChild != NULL)
+    {
+        Node* tempParent = nodeToRemove;
+        Node* nodeToEmplace = tempParent->rightChild;
+        for (; nodeToEmplace->leftChild != NULL; 
+            tempParent = nodeToEmplace, nodeToEmplace = nodeToEmplace->leftChild);
+        movePointers(dictionary, nodeToEmplace, tempParent);
+        free(nodeToRemove->value);
+        nodeToRemove->key = nodeToEmplace->key;
+        nodeToRemove->value = nodeToEmplace->value;
+        free(nodeToEmplace);
+    }
+    else
+    {
+        movePointers(dictionary, nodeToRemove, parent);
+        free(nodeToRemove->value);
+        free(nodeToRemove);
+    }
+}
+
+void deleteDictionary(Dictionary** const dictionary)
+{
+    if (dictionary == NULL || *dictionary == NULL)
+    {
+        return;
+    }
+    while ((*dictionary)->root != NULL)
+    {
+        removeKey(*dictionary, (*dictionary)->root->key);
+    }
+    free(*dictionary);
+    *dictionary = NULL;
+}
